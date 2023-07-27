ae8f05a2c63a2871c48866dbc9cc82ed78e9437e
markMintel <101613208+markMintel@users.noreply.github.com>
2023-07-27 10:41:49 -0400

    Update app.py
    testing fetchall

--- app.py
+++ app.py
@@ -2,13 +2,17 @@
 
 import streamlit as st
 import snowflake.connector
+import pandas as pd
 
 st.title("Zena's Amazing Athleisure Catalog")
 st.text('Pick a sweatsuit color or style:')
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+
+my_cur.execute("select color_or_style from zenas_athleisure_db.products.catalog_for_website")
+catalog = my_cur.fetchall()
+
+df = pd.DataFrame(catalog)
+st.write(df)
+
