85fd4f20812e541bd48620edeab60edfca35809b
markMintel <101613208+markMintel@users.noreply.github.com>
2023-07-27 10:53:19 -0400

    Update app.py
    Add remaining app displays

--- app.py
+++ app.py
@@ -15,6 +15,15 @@ catalog = my_cur.fetchall()
 df = pd.DataFrame(catalog)
 colors = df[0].values.tolist()
 
-dropbox = st.selectbox('Pick a sweatsuit color or style:', list(colors))
+option = st.selectbox('Pick a sweatsuit color or style:', list(colors))
+product_caption = 'Our warm, comfortable, '+ option +' sweatsuit
+
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from zenas_athleisure_db.products.catalog_for_website where color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+
+st.image(df2[0],width=400, caption=product_caption)
+st.write('Price:',df2[1])
+st.write('Sizes Available:',df2[2])
+st.write(df2[3])
 
 
